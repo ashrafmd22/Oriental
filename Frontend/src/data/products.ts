5329226203b5b3248e974749d6ec37d4df045956
Md Ashraf <ashhiimd@gmail.com>
2024-12-28 02:01:49 +0530

    lat

--- Frontend/src/data/products.ts
+++ Frontend/src/data/products.ts
@@ -16,7 +16,7 @@ export const products: Product[] = [
       "Water-resistant"
     ],
     "images": [
-      "/assets/Bags/1.jpg",
+      "assets/Bags/1.jpg",
       "/assets/Bags/2.jpg",
       "/assets/Bags/3.jpg"
     ]
